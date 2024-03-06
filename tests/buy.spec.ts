c567b2233cbbebaba1e4862614813ed7cf92abde
Santosh Kalidindi <skalidindi8@gmail.com>
2024-03-06 08:12:11 -0800

    fix: add consoles and wait for buy button

--- tests/buy.spec.ts
+++ tests/buy.spec.ts
@@ -21,7 +21,7 @@ test("buy my dam router", async ({ page }) => {
 
   await expect(page.getByRole("button", { name: "Add to Cart" })).toBeVisible();
 
-  // Start buying process
+  console.log("Add to cart button is visible... starting buying process");
   await page.getByRole("button", { name: "Add to Cart" }).click();
   await page.waitForLoadState("networkidle");
 
@@ -70,6 +70,8 @@ test("buy my dam router", async ({ page }) => {
 
   const stripe3rdFrame = page.frameLocator("iframe").nth(2);
   await stripe3rdFrame.locator('[placeholder="CVC"]').fill(CC_CVC);
+
+  await page.waitForTimeout(1000);
 
   await page.getByRole("button", { name: "Pay Now" }).click();
 });
